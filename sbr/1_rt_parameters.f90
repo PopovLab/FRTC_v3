c36435eea111d28f8539e052c2b072e59e8777f8
Alex <temper8@gmail.com>
2023-10-23 11:26:56 +0300

    Revert "add wave_mode var"
    This reverts commit 7c94eebe42c035408f7bd4f35fb33eeb646de582.

--- sbr/1_rt_parameters.f90
+++ sbr/1_rt_parameters.f90
@@ -77,7 +77,7 @@ module rt_parameters
 !!!!!!!!!!!!!  options !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     integer ::  ipri
     !! ipri, printing output monitoring: 0,1,2,3,4
-    integer ::  iw, wave_mode
+    integer ::  iw
     !! iw, initial mode (slow=1, fast=-1)
     integer ::  ismth
     !! ismth, if=0, no smoothing in Ne(rho),Te(rho),Ti(rho)
@@ -176,7 +176,6 @@ module rt_parameters
                read(iunit,*)
                read(iunit,*) ipri
                read(iunit,*) iw
-               wave_mode = iw
                read(iunit,*) ismth
                read(iunit,*) ismthalf
                read(iunit,*) ismthout
