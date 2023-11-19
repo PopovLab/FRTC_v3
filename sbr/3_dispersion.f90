af6ff7bc545bcb254185eeeb003f3019bb1c832a
Alex <temper8@gmail.com>
2023-11-19 19:33:58 +0300

    Update 3_dispersion.f90

--- sbr/3_dispersion.f90
+++ sbr/3_dispersion.f90
@@ -622,7 +622,7 @@ contains
     subroutine disp2_iroot3(pa, yn2, ptet, xnro, pg1, pg2, pg3, pg4)
         ! case iroot == 3  ivar=0
         use constants, only: zero, one, two
-        use rt_parameters, only: inew, iw, itend0, kv
+        use rt_parameters, only: iw
         use metrics
         use dielectric_tensor
         use dispersion_equation
@@ -718,12 +718,7 @@ contains
     subroutine disp2_iroot2(pa,yn2,ptet,prt,prm)
         ! case iroot == 2
         use constants, only: zero, one, two
-        use constants, only: c0, c1,pi
-        use constants, only: zalfa, xmalfa, xlog, clt
-        use plasma, only: fn1, fn2, fvt, ft, zefff
-        use plasma, only: ww, xmi,xsz, cltn, cnye, cnyi
-        use plasma, only: cnstal, valfa, vperp
-        use rt_parameters, only: inew, iw, itend0, kv
+        use rt_parameters, only: iw
         use metrics
         use dielectric_tensor
         use dispersion_equation
